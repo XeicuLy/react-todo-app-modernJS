fd543f3d52b328cd4290906d5211022e448240da
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-25 16:52:14 +0900

    :sparkles:feat: 基本画面の作成
    Reactを使って動的にリストを出力する方法で基本ページを作成しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inCompleteTodos, setInCompleteTodos] = useState(['ああああ', 'いいいい']);
+  const [completeTodos, setCompleteTodos] = useState(['うううう']);
+
   return (
     <>
-      <h1>Hello React</h1>
-      <p>Todo</p>
+      <div className='input-box'>
+        <input className='input-area' type='text' placeholder='TODOを入力' />
+        <button className='button'>追加</button>
+      </div>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
+        <ul className='incomplete-lists'>
+          {inCompleteTodos.map(todo => {
+            return (
+              <li key={todo} className='list incomplete-list'>
+                <p className='description'>{todo}</p>
+                <button className='button complete-button'>完了</button>
+                <button className='button delete-button'>削除</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
+        <ul className='complete-lists'>
+          {completeTodos.map(todo => {
+            return (
+              <li key={todo} className='list complete-list'>
+                <p className='description'>{todo}</p>
+                <button className='button return-button'>戻す</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
